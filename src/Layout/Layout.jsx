af6c22171ce382bbc2160091056fcb2439878c80
Daniil Popov <x6uhrox@gmail.com>
2023-05-07 22:45:41 +0300

    FIX: lazy load pages

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,46 +1,60 @@
-import { RotatingLines } from  'react-loader-spinner'
+import { RotatingLines } from "react-loader-spinner";
 import { Container, Centered } from "./Layout.styled";
 import { NavLink } from "react-router-dom";
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Layout = () => {
-return (
+  const [showFallback, setShowFallback] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowFallback(true);
+    }, 250);
+  }, []);
+
+  return (
     <Container>
-    <header>
-     <nav>
-        <ul>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/tweets">Tweets</NavLink></li>
-        </ul>
-     </nav>
-    </header>
-    <main>
-    
-      <Suspense
-        fallback={
-            <Centered>
-            <RotatingLines
-            strokeColor="#ebd8ff"
-            strokeWidth="5"
-            animationDuration="0.75"
-            width="96"
-            visible={true}
-          />
-          </Centered>
-        }
-      >
-        <Outlet />
-      </Suspense>
-    </main>
-    <footer>
-      {/* <FooterWrappper>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/tweets">Tweets</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Suspense
+          fallback={
+            showFallback && (
+              <Centered>
+                <RotatingLines
+                  strokeColor="#ebd8ff"
+                  strokeWidth="5"
+                  animationDuration="0.75"
+                  width="96"
+                  visible={true}
+                />
+              </Centered>
+            )
+          }
+        >
+          <Outlet />
+        </Suspense>
+      </main>
+      <footer>
+        {/* <FooterWrappper>
         <Copyright>©2023 Daniil Popov</Copyright>
         <MailLink href="mailto:x6uhrox@gmail.com">x6uhrox@gmail.com</MailLink>
       </FooterWrappper> */}
-    </footer>
-  </Container>
-)
-}
+      </footer>
+    </Container>
+  );
+};
 
 export default Layout;
