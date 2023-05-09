a2af577a2a52139c4f24a126158cba330b89fbfa
Daniil Popov <x6uhrox@gmail.com>
2023-05-09 13:01:04 +0300

    FIX: 404

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
+import { lazy } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Layout from "./Layout/Layout";
 
-import { lazy } from 'react';
-import { Route, Routes, Navigate, HashRouter } from 'react-router-dom';
-import Layout from './Layout/Layout';
-
-const Home = lazy(() => import('./pages/Home/Home'));
-const Tweets = lazy(() => import('./pages/Tweets/Tweets'));
+const Home = lazy(() => import("./pages/Home/Home"));
+const Tweets = lazy(() => import("./pages/Tweets/Tweets"));
 
 function App() {
   return (
     <Routes>
-       <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
-        </Route>
         <Route path="*" element={<Navigate to="/" />} />
+      </Route>
     </Routes>
   );
 }
